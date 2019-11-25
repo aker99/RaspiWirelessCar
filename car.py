0e4380815ed25791b273743cd4551daff6c84e23
Himanshu Kotnala <himanshu.kotnala99@gmail.com>
2019-11-25 21:28:06 +0530

    Live Stream and Minnor Bug Fix

--- car.py
+++ car.py
@@ -1,49 +1,49 @@
-#import RPi.GPIOas GPIO# Import Raspberry Pi GPIOlibrary
+import RPi.GPIO as GPIO #Import Raspberry Pi GPIOlibrary
 
 class Motion():
 	"""docstring for ClassName"""
 	def __init__(self):
 		print("contructor")
-		#GPIO.setwarnings(False) # Ignore warning for now
-		#GPIO.setmode(#GPIO.BOARD) # Use physical pin numbering
-		#GPIO.setup(16, #GPIO.OUT, initial=#GPIO.LOW)#right
-		#GPIO.setup(18, #GPIO.OUT, initial=#GPIO.LOW)#right
-		#GPIO.setup(13, #GPIO.OUT, initial=#GPIO.LOW)#left 
-		#GPIO.setup(15, #GPIO.OUT, initial=#GPIO.LOW)#left
+		GPIO.setwarnings(False) # Ignore warning for now
+		GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+		GPIO.setup(31, GPIO.OUT, initial=GPIO.LOW)#right
+		GPIO.setup(33, GPIO.OUT, initial=GPIO.LOW)#right
+		GPIO.setup(37, GPIO.OUT, initial=GPIO.LOW)#left 
+		GPIO.setup(35, GPIO.OUT, initial=GPIO.LOW)#left
 		
 	def forward(self):
 		print("forward2")
-		#GPIO.output(16, #GPIO.LOW)#a2
-		#GPIO.output(18, #GPIO.HIGH)#a1
-		#GPIO.output(13, #GPIO.HIGH)#b2
-		#GPIO.output(15, #GPIO.LOW)#b1
+		GPIO.output(31, GPIO.LOW)#a2
+		GPIO.output(33, GPIO.HIGH)#a1
+		GPIO.output(37, GPIO.HIGH)#b2
+		GPIO.output(35, GPIO.LOW)#b1
 
 	def backward(self):
 		print("backward2")
-		#GPIO.output(16, #GPIO.HIGH)#a2
-		#GPIO.output(18, #GPIO.LOW)#a1
-		#GPIO.output(13, #GPIO.LOW)#b2
-		#GPIO.output(15, #GPIO.HIGH)#b1
+		GPIO.output(31, GPIO.HIGH)#a2
+		GPIO.output(33, GPIO.LOW)#a1
+		GPIO.output(37, GPIO.LOW)#b2
+		GPIO.output(35, GPIO.HIGH)#b1
 
 	def left(self):
 		print("left2")
-		#GPIO.output(16, #GPIO.HIGH)#a2
-		#GPIO.output(18, #GPIO.LOW)#a1
-		#GPIO.output(13, #GPIO.HIGH)#b2
-		#GPIO.output(15, #GPIO.LOW)#b1
+		GPIO.output(31, GPIO.HIGH)#a2
+		GPIO.output(33, GPIO.LOW)#a1
+		GPIO.output(37, GPIO.HIGH)#b2
+		GPIO.output(35, GPIO.LOW)#b1
 
 	def right(self):
 		print("right2")
-		#GPIO.output(16, #GPIO.LOW)#a2
-		#GPIO.output(18, #GPIO.LOW)#a1
-		#GPIO.output(13, #GPIO.LOW)#b2
-		#GPIO.output(15, #GPIO.LOW)#b1
+		GPIO.output(31, GPIO.LOW)#a2
+		GPIO.output(33, GPIO.HIGH)#a1
+		GPIO.output(37, GPIO.LOW)#b2
+		GPIO.output(35, GPIO.HIGH)#b1
 
 	def stop(self):
 		print("stop2")
-		#GPIO.output(16, #GPIO.LOW)#a2
-		#GPIO.output(18, #GPIO.LOW)#a1
-		#GPIO.output(13, #GPIO.LOW)#b2
-		#GPIO.output(15, #GPIO.LOW)#b1
+		GPIO.output(31, GPIO.LOW)#a2
+		GPIO.output(33, GPIO.LOW)#a1
+		GPIO.output(37, GPIO.LOW)#b2
+		GPIO.output(35, GPIO.LOW)#b1
 
 motion = Motion()
--- controller.py
+++ controller.py
@@ -1,5 +1,9 @@
-from flask import Flask , request, flash, redirect, jsonify
+from flask import Flask ,render_template, request, flash, redirect, jsonify
 from car import motion 
+import socket
+
+localip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+print(localip)
 
 app = Flask(__name__)
 
@@ -22,21 +26,25 @@ def forward():
 @app.route('/backward')
 def backward():
 	print("backward")
+	motion.backward()
 	return do_return('OK',200)
 
 @app.route('/left')
 def left():
 	print("left")
+	motion.left()
 	return do_return('OK',200)
 
 @app.route('/right')
 def right():
-	print("forward")
+	print("right")
+	motion.right()
 	return do_return('OK',200)
 
 @app.route('/stop')
 def stop():
 	print("stop")
+	motion.stop()
 	return do_return('OK',200)
 
 if __name__ == '__main__':
